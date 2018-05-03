bba6ed518a448bd0f21e63fd8af746dbf8e08f55
John McDermott <jem@atmc.net>
2018-05-03 15:09:58 -0400

    Drawing a circle

---
+++ Playing Card/PlayingCardView.swift
@@ -0,0 +1,31 @@
+//
+//  PlayingCardView.swift
+//  Playing Card
+//
+//  Created by John McDermott on 5/3/18.
+//  Copyright © 2018 John McDermott. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+
+//        if let context = UIGraphicsGetCurrentContext() {
+//            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//            context.setLineWidth(5.0)
+//            UIColor.green.setFill()
+//            UIColor.red.setStroke()
+//            context.strokePath()
+//            context.fillPath()
+//        }
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.green.setFill()
+        UIColor.red.setStroke()
+        path.stroke()
+        path.fill()
+    }
+}
