3242c164e65fad7bcbea71069db0f6528631d6fb
John McDermott <jem@atmc.net>
2018-05-03 22:18:41 -0400

    Stopping point

--- Playing Card/PlayingCardView.swift
+++ Playing Card/PlayingCardView.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class PlayingCardView: UIView {
 
+    var rank: Int = 5 { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    var suit: String = "♥️" { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    var isFaceUp: Bool = true { didSet { setNeedsDisplay(); setNeedsLayout() } }
+
+    private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        return NSAttributedString(string: string, attributes: [.paragraphStyle:paragraphStyle, .font:font])
+    }
+
+    private var cornerString: NSAttributedString {
+        return centeredAttributedString(rank+"\n"+suit, fontSize: 0.0)
+    }
+
     override func draw(_ rect: CGRect) {
         let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
         roundedRect.addClip()
